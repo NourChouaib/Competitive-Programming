d003b12e8f031078c5f9900a50109a73b94068e4
nourshoaib <nourchouaib.dev@gmail.com>
2026-03-06 23:41:05 +0200

    Finishing Concert Tickets

--- CSES/SortingAndSearching/ConcertTickets.cpp
+++ CSES/SortingAndSearching/ConcertTickets.cpp
@@ -1,38 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int search(vector<int> arr, int n, int target){
-    int left = 0;
-    int right = n - 1;
-    int result = -1;
-    while (left <= right){
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target){
-            return mid;
-        }else if(arr[mid] < target){
-            result = mid;
-            left = mid + 1;
-        }else{
-            right = mid - 1; 
-        }
-    }
-    return result;
-}
 
 int solve(){
     int n,m; cin >> n >> m; // n : nb of tickets
-    vector<int> h(n); for(int &x : h) cin >> x; // price of tickets
-    vector<int> t(m); for(int &x : t) cin >> x; 
+    multiset<int> h; // price of tickets
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        h.insert(x);
+    }
 
-    sort(h.begin(),h.end());
-
-    for(int i = 0; i < m; i++){
-        int ans = search(h,n - i,t[i]);
-        if ( ans != -1){
-            cout << h[ans] << endl;
-            h.erase(h.begin() + ans);
-        }else{
+    
+    for(int i = 0; i < m; i++){ // O(m)
+        int t;
+        cin >> t;
+        auto ans = h.upper_bound(t); // O(log(n))
+        if ( ans == h.begin()){
             cout << -1 << endl;
+        }else{
+            ans--;
+            cout << *ans << endl;
+            h.erase(ans);
         }
     }
     return 0;
