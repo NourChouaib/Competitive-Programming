5e96d994d1ee0fc6ad006df9a52bb2fcbd9aee78
nourshoaib <nourchouaib.dev@gmail.com>
2026-03-11 23:17:05 +0200

    solve

---
+++ CodeForces/800/TrippiTroppi.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(){
+    char ans[3];
+    for(int i = 0; i<3; i++){
+        char s[10];
+        cin >> s;
+        cout << s[0];
+    }
+    cout << endl;
+    return 0;
+}
+
+int main(){
+    int t;cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
