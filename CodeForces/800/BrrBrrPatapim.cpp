7796a9d7e2c8ad5b8ff5fc27c5db56fd2cfe9c85
nourshoaib <nourchouaib.dev@gmail.com>
2026-03-17 20:15:37 +0200

    edit code

--- CodeForces/800/BrrBrrPatapim.cpp
+++ CodeForces/800/BrrBrrPatapim.cpp
@@ -1,42 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int solve(){
-    int n; cin >> n;
-    vector<vector<int>> grid(n, vector<int>(n));
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> grid[i][j];
-        }
-    }
-
-    vector<pair<int,int>>a(2*n + 1);
-    for(int i=1;i<2*n + 1;i++){
-        a[i] = {i,0};
-    }
-
-    for(int i = 1;i < n + 1; i++){
-        for(int j = 1; j < n + 1; j++){
-            int val = grid[i - 1][j - 1];
-            a[val].second = i + j;
-        }
-    }
-
-    sort(a.begin(),a.end(),[] (pair<int,int> a, pair<int, int> b){
-    return a.second < b.second;});
-
-    for(int i = 1; i < 2*n + 1; i++){
-        cout << a[i].first << " ";
-    }
-
-    return 0;
-}
-
-int main(){
-    int t;cin >> t;
+int main()
+{
+    int t; cin >> t;
     while(t--){
-        solve();
+        int n; cin >> n;
+        vector <int> a(2*n - 1,0);
+        vector <int> check(2*n + 1,0);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int x; cin >> x;
+                a[i + j] = x;
+                check[a[i + j]] = 1;
+            }
+        }
+        
+        for(int i = 1; i < 2*n + 1; i++){
+            if (check[i] == 0){
+                cout << i << " ";
+            }
+        }
+        for(int &k: a){
+            cout << k << " ";
+        }
+        cout << endl;
+        
     }
+
     return 0;
 }
