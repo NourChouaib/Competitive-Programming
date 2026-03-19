e8aac21331805cfafcbf5d9d3054452f6864ed3f
nourshoaib <nourchouaib.dev@gmail.com>
2026-03-19 23:30:20 +0200

    solve

---
+++ CodeForces/971_Div4/Minimize.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(){
+    int a,b; cin >> a >> b;
+    cout << b-a << endl;
+    return 0;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
