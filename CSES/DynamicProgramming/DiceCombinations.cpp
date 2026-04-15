a124c531b3b9ac0d351bbbfb33d6c6a636c1d3c1
nourshoaib <nourchouaib.dev@gmail.com>
2026-04-15 20:32:40 +0300

    Solving First dp problem

--- CSES/DynamicProgramming/DiceCombinations.cpp
+++ CSES/DynamicProgramming/DiceCombinations.cpp
@@ -1,23 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long 
-
+#define ll long long 
+const ll MOD = 1e9 + 7;
 
 
 void solve(){
     int n; cin >> n;
-    vector <int> dp(n + 1,0);
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 4;
-    dp[4] = 8;
-    dp[5] = 16;
-    dp[6] = 
+    vector <ll> dp(n + 1,0);
 
-    
+    dp[0] = 1;
+    ll sum = 1;
+    int last = (n < 6)? n: 6;
+    for(int i = 1; i <= last; i++){
+        dp[i] = sum % MOD;
+        sum += dp[i];
+    }
+
+    for(int i=7; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4]+ dp[i-5]+ dp[i-6])%MOD;
+    }
+
+    cout << dp[n]%MOD << "\n";
 }
 
 int main(){
     solve();
+    return 0;
 }
